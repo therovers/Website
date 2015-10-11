83ff9a35588e3de64bcae5664a0482da1f7a95ab
jyotsnagorle <jyotsnagorle@gmail.com>
2015-10-11 14:57:00 +0530

    JG -changed heritage to support + changed config

--- dist/assets/config/stories_source.js
+++ dist/assets/config/stories_source.js
@@ -2,18 +2,18 @@
   window.getStories = function() {
     return [
       {
-        heading: "Thailand tour stories",
-        desc: "and living parallel processes to stimulate growth in an individual developing a sense of pride. We want to make travel a mode of educating and living parallel processes to stimulate growth in an individual developing a and living parallel processes to stimulate growth in an individual developing a instead of leisure by engaging students in group workshops, individual activities which would bring out the best in each.",
+        heading: "WAT PHO, Bangkok",
+        desc: "Bangkok, the capital of Thailand is famous for many temples of which Wat Pho is one of the most famous belonging to first grade royal monasteries built under the reign of King Rama I of Chakri dynasty. Built in Thai architectural style, it is famous for the huge 43m long reclining statue of Buddha and also UNESCO memory of the world listed inscriptions. This place is regarded as birthplace of Thai massage as well.",
         link: "thailand_story.html",
         image: "3.jpeg"
       }, {
-        heading: "Heading 2",
-        desc: "Desc 2",
+        heading: "WAT PHNOM, Phnom Penh",
+        desc: "Wat Phnom (Mountain Pagoda) is a sacred Buddhist pagoda located in Phnom Penh, Cambodia. It is the tallest religious structure in the city as it stands on a small hill. The pagoda was earlier known by the name of Wat Preah Chedey Borapaut.",
         link: "link2.html",
         image: "1.jpeg"
       }, {
-        heading: "Heading 3",
-        desc: "Desc 3",
+        heading: "Sanctuary of Truth, Pattaya ",
+        desc: "The Sanctuary of Truth is a gigantic wooden construction which covers the area of more than two rais by the sea at Rachavate Cape at the northern end of Pattaya Bay.  The huge teak building is 105 m high.",
         link: "link3.html",
         image: "2.jpeg"
       }

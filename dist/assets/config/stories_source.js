27fa6453b7da396ee6a867603e62a0c26f3275b5
jyotsnagorle <jyotsnagorle@gmail.com>
2015-10-28 23:40:22 +0530

    jg - wat pho and phnom pages and phnom text

--- dist/assets/config/stories_source.js
+++ dist/assets/config/stories_source.js
@@ -4,17 +4,17 @@
       {
         heading: "Wat Pho, Bangkok",
         desc: "Bangkok, the capital of Thailand is famous for many temples of which Wat Pho is one of the most famous belonging to first grade royal monasteries built under the reign of King Rama I of Chakri dynasty. Built in Thai architectural style, it is famous for the huge 43m long reclining statue of Buddha and also UNESCO memory of the world listed inscriptions. This place is regarded as birthplace of Thai massage as well.",
-        link: "thailand_story.html",
+        link: "wat_pho.html",
         image: "wat_pho.jpg"
       }, {
         heading: "Wat Phnom, Phnom Penh",
         desc: "Wat Phnom (Mountain Pagoda) is a sacred Buddhist pagoda located in Phnom Penh, Cambodia. It is the tallest religious structure in the city as it stands on a small hill. The pagoda was earlier known by the name of Wat Preah Chedey Borapaut.",
-        link: "link2.html",
+        link: "wat_phnome.html",
         image: "wat_phnom.jpg"
       }, {
         heading: "Sanctuary of Truth, Pattaya ",
         desc: "The Sanctuary of Truth is a gigantic wooden construction which covers the area of more than two rais by the sea at Rachavate Cape at the northern end of Pattaya Bay.  The huge teak building is 105 m high.",
-        link: "link3.html",
+        link: "thailand_story.html",
         image: "truth.jpg"
       }
     ];

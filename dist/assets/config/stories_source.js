6cf5c89ec8feead46e7fee2c746deb9ef155be9d
JyotsnaGorle <jyotsnagorle@gmail.com>
2015-11-20 22:41:11 +0530

    JG - added delhi story to home page

--- dist/assets/config/stories_source.js
+++ dist/assets/config/stories_source.js
@@ -16,6 +16,11 @@
         desc: "The Sanctuary of Truth is a gigantic wooden construction which covers the area of more than two rais by the sea at Rachavate Cape at the northern end of Pattaya Bay.  The huge teak building is 105 m high.",
         link: "thailand_story.html",
         image: "truth.jpg"
+      }, {
+        heading: "New Delhi, India ",
+        desc: "New Delhi, our very own National Capital which sits on reformed lands of 7 ancient cities, is an architectural treasure, although the number of cities, it is believed to be an amalgamation of, is highly debatable. From Pandavas's Indraprastha to Lutyens's Delhi, the nature of each city that is contained within, is significantly different from one another.Although some would argue that it is not possible to grasp its architectural magnificence in such a short time, it is also necessary to point out that no amount of time is enough to understand the city and its history. From history to politics, this city has been significantly prominent throughout most periods of development of the country almost in every field, architecture being one of the various. The purpose of the stopover at the Capital further narrows down to how the city has modified itself over the course of history to be home to so many cultures, thus owing the reason of its rich heritage and architectural magnificence to it. Architecture belongs to culture, not civilization - said Alvar Aalto.",
+        link: "delhi.html",
+        image: "delhi_3.jpg"
       }
     ];
   };

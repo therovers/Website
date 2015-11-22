ee3521f1bbf6b63af4cb53e03b1f3c17ce08ff3e
JyotsnaGorle <jyotsnagorle@gmail.com>
2015-11-22 23:53:14 +0530

    added links from souvenirs section to souvenirs page

--- dist/assets/config/stories_source.js
+++ dist/assets/config/stories_source.js
@@ -4,22 +4,22 @@
       {
         heading: "Wat Pho, Bangkok",
         desc: "Bangkok, the capital of Thailand is famous for many temples of which Wat Pho is one of the most famous belonging to first grade royal monasteries built under the reign of King Rama I of Chakri dynasty. Built in Thai architectural style, it is famous for the huge 43m long reclining statue of Buddha and also UNESCO memory of the world listed inscriptions. This place is regarded as birthplace of Thai massage as well.",
-        link: "wat_pho.html",
+        link: "souvenirs.html",
         image: "wat_pho.jpg"
       }, {
         heading: "Wat Phnom, Phnom Penh",
         desc: "Wat Phnom (Mountain Pagoda) is a sacred Buddhist pagoda located in Phnom Penh, Cambodia. It is the tallest religious structure in the city as it stands on a small hill. The pagoda was earlier known by the name of Wat Preah Chedey Borapaut.",
-        link: "wat_phnome.html",
+        link: "souvenirs.html",
         image: "wat_phnom.jpg"
       }, {
         heading: "Sanctuary of Truth, Pattaya ",
         desc: "The Sanctuary of Truth is a gigantic wooden construction which covers the area of more than two rais by the sea at Rachavate Cape at the northern end of Pattaya Bay.  The huge teak building is 105 m high.",
-        link: "thailand_story.html",
+        link: "souvenirs.html",
         image: "truth.jpg"
       }, {
         heading: "New Delhi, India ",
-        desc: "New Delhi, our very own National Capital which sits on reformed lands of 7 ancient cities, is an architectural treasure, although the number of cities, it is believed to be an amalgamation of, is highly debatable. From Pandavas's Indraprastha to Lutyens's Delhi, the nature of each city that is contained within, is significantly different from one another.Although some would argue that it is not possible to grasp its architectural magnificence in such a short time, it is also necessary to point out that no amount of time is enough to understand the city and its history. From history to politics, this city has been significantly prominent throughout most periods of development of the country almost in every field, architecture being one of the various. The purpose of the stopover at the Capital further narrows down to how the city has modified itself over the course of history to be home to so many cultures, thus owing the reason of its rich heritage and architectural magnificence to it. Architecture belongs to culture, not civilization - said Alvar Aalto.",
-        link: "delhi.html",
+        desc: "New Delhi, our very own National Capital which sits on reformed lands of 7 ancient cities, is an architectural treasure, although the number of cities, it is believed to be an amalgamation of, is highly debatable. From Pandavas's Indraprastha to Lutyens's Delhi, the nature of each city that is contained within, is significantly different from one another.Although some would argue that it is not possible to grasp its architectural magnificence in such a short time, it is also necessary to point out that no amount of time is enough to understand the city and its history. From history to politics.",
+        link: "souvenirs.html",
         image: "delhi_3.jpg"
       }
     ];
--- src/assets/config/stories_source.coffee
+++ src/assets/config/stories_source.coffee
@@ -3,25 +3,24 @@ window.getStories = ->
     {
       heading: "Wat Pho, Bangkok"
       desc: "Bangkok, the capital of Thailand is famous for many temples of which Wat Pho is one of the most famous belonging to first grade royal monasteries built under the reign of King Rama I of Chakri dynasty. Built in Thai architectural style, it is famous for the huge 43m long reclining statue of Buddha and also UNESCO memory of the world listed inscriptions. This place is regarded as birthplace of Thai massage as well."
-      link: "wat_pho.html"
+      link: "souvenirs.html"
       image: "wat_pho.jpg"
     }
     {
       heading: "Wat Phnom, Phnom Penh"
       desc: "Wat Phnom (Mountain Pagoda) is a sacred Buddhist pagoda located in Phnom Penh, Cambodia. It is the tallest religious structure in the city as it stands on a small hill. The pagoda was earlier known by the name of Wat Preah Chedey Borapaut."
-      link: "wat_phnome.html"
+      link: "souvenirs.html"
       image: "wat_phnom.jpg"
     }
     {
       heading: "Sanctuary of Truth, Pattaya "
       desc: "The Sanctuary of Truth is a gigantic wooden construction which covers the area of more than two rais by the sea at Rachavate Cape at the northern end of Pattaya Bay.  The huge teak building is 105 m high."
-      link: "thailand_story.html"
+      link: "souvenirs.html"
       image: "truth.jpg"
     }
     {
       heading: "New Delhi, India "
       desc: "New Delhi, our very own National Capital which sits on reformed lands of 7 ancient cities, is an architectural treasure, although the number of cities, it is believed to be an amalgamation of, is highly debatable. From Pandavas's Indraprastha to Lutyens's Delhi, the nature of each city that is contained within, is significantly different from one another."
-
       link: "delhi.html"
       image: "delhi_3.jpg"
     }
